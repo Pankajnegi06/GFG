33979cd6537e65abf52b4785bcd8a8565e65ddf0
Pankaj Singh Negi <158542630+Pankajnegi06@users.noreply.github.com>
2026-07-06 09:50:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Replace Consecutive Two Same with One/replace-consecutive-two-same-with-one.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        if (s.length() == 0) return "";
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(s.charAt(0));
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) != s.charAt(i - 1)) {
+                sb.append(s.charAt(i));
+            }
+        }
+
+        return sb.toString();
+    }
+}
